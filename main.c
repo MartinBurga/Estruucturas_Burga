11c035c93026f43be390de8e518c9495a98357b2
Martin Burga <martin.burga@udla.edu.ec>
2024-01-17 22:53:55 -0500

    Arreglo de errores y adicion de la opcion 1 (opcion en la que se digita los datos de cada estudiante que anteriormente daba problema. Se corrigio y se reinterpeto el struct que paso a estar afuera para que sus variables sean globales

--- main.c
+++ main.c
@@ -3,19 +3,20 @@
 int opcion;
 int num;
 
+struct alumno
+{
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char carrera[50];
+    float promedio;
+};
+
+struct alumno alumnos[100];
+
 int main()
 {
-
-    struct alumno
-    {
-        int matricula;
-        char nombre[50];
-        char direccion[50];
-        char carrera[50];
-        float promedio;
-    };
-
-    printf("Bienvenido al registro de alumnos. Inserta la opción que deseas realizar.\n");
+    printf("Bienvenido al registro de alumnos. Inserta la opcion que deseas realizar.\n");
     printf("1. Ingresar datos.\n");
     printf("2. Imprimir datos.\n");
     scanf("%d", &opcion);
@@ -24,17 +25,37 @@ int main()
     {
         if (opcion == 1)
         {
-            printf("Ingresar el nombre del estudiante\n");
+            printf("Ingrese la cantidad de alumnos:\n");
+            scanf("%d", &num);
+
+            for (int i = 0; i < num; ++i)
+            {
+                printf("Ingrese los datos del alumno %d:\n", i + 1);
+
+                printf("Ingrese la matricula del alumno: ");
+                scanf("%d", &alumnos[i].matricula);
+
+                printf("Ingrese el nombre del alumno: ");
+                scanf("%s", alumnos[i].nombre);
+
+                printf("Ingrese la direccion del alumno: ");
+                scanf("%s", alumnos[i].direccion);
+
+                printf("Ingrese la carrera del alumno: ");
+                scanf("%s", alumnos[i].carrera);
+
+                printf("Ingrese el promedio del alumno: ");
+                scanf("%f", &alumnos[i].promedio);
+            }
         }
         else if (opcion == 2)
         {
-
             printf("Datos de los alumnos ingresados:\n");
             for (int i = 0; i < num; ++i)
             {
-                printf("\nMatrícula: %d\n", alumnos[i].matricula);
+                printf("Matricula: %d\n", alumnos[i].matricula);
                 printf("Nombre: %s\n", alumnos[i].nombre);
-                printf("Dirección: %s\n", alumnos[i].direccion);
+                printf("Direccion: %s\n", alumnos[i].direccion);
                 printf("Carrera: %s\n", alumnos[i].carrera);
                 printf("Promedio: %.2f\n", alumnos[i].promedio);
             }
@@ -44,6 +65,7 @@ int main()
         printf("1. Insertar datos.\n");
         printf("2. Imprimir los datos.\n");
         printf("0. Salir.\n");
+        scanf("%d", &opcion);
     } while (opcion != 0);
 
     return 0;
